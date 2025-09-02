4c062d837b10bb569ac9103d13274719a807c945
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-02 14:23:55 +0800

    a16

---
+++ crdroid-12/daisy_a16_recipe.sh
@@ -0,0 +1,32 @@
+git clone --depth=1 --branch lineage-22.2-re https://github.com/ximi-daisy-test/android_device_xiaomi_daisy device/xiaomi/daisy
+
+git clone --depth=1 --branch lineage-22.2-re https://github.com/ximi-daisy-test/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
+
+git clone --depth=1 --branch lineage-22.2 https://github.com/ximi-daisy-test/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+
+git clone --depth=1 --branch lineage-21 https://github.com/ximi-daisy-test/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
+
+git clone --depth=1 --branch lineage-22.2 https://github.com/ximi-daisy-test/proprietary_vendor_xiaomi_daisy vendor/xiaomi/daisy
+
+git clone --depth=1 --branch lineage-23 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+
+git clone --depth=1 --branch lineage-22.2 https://github.com/LineageOS/android_hardware_sony_timekeep hardware/sony/timekeep
+
+
+# kernel tree
+# https://github.com/ximi-daisy-test/android_kernel_xiaomi_msm8953/tree/aff8387c5c16b37de3fed12cd0008c7cb9cb5acd
+
+
+git clone --depth=1 https://github.com/LineageOS/android_external_ant-wireless_ant_client.git external/ant-wireless/ant_client
+git clone --depth=1 https://github.com/LineageOS/android_external_ant-wireless_ant_native.git external/ant-wireless/ant_native
+git clone --depth=1 https://github.com/LineageOS/android_external_ant-wireless_ant_service.git external/ant-wireless/ant_service
+
+
+# fixes
+# https://github.com/LineageOS/android_external_ant-wireless_ant_service/commit/808676c8bf84dddec22c76fdff880e6e394c366e
+# https://github.com/kamikaonashi/private_xiaomi_mido/commit/8266177e861f88841bfe609da3bdc127b7653859
+
+
+# to fix
+# livedisplay where
+
