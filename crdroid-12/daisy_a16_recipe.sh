d69a8283b7bbceee85dd5ef433b3caf8f9dbc3bc
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-18 15:36:07 +0800

    crdroid-12: update manifest

--- crdroid-12/daisy_a16_recipe.sh
+++ crdroid-12/daisy_a16_recipe.sh
@@ -1,10 +1,10 @@
-git clone --depth=1 --branch lineage-22.2 https://github.com/ximi-daisy-test/android_device_xiaomi_daisy device/xiaomi/daisy
+git clone --depth=1 --branch 16 https://github.com/ximi-daisy-test/android_device_xiaomi_daisy device/xiaomi/daisy
 
-git clone --branch lineage-22.2-re https://github.com/ximi-daisy-test/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
+git clone --depth=1 --branch 16 https://github.com/ximi-daisy-test/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
 
 git clone --depth=1 --branch 16 https://github.com/ximi-daisy-test/proprietary_vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
 
-git clone --depth=1 --branch lineage-22.2 https://github.com/ximi-daisy-test/proprietary_vendor_xiaomi_daisy vendor/xiaomi/daisy
+git clone --depth=1 --branch 16 https://github.com/ximi-daisy-test/proprietary_vendor_xiaomi_daisy vendor/xiaomi/daisy
 
 git clone --depth=1 --branch lineage-23.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
