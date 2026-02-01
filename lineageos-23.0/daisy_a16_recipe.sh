e08de2961b57fb45bcd70b07271e88509f26234f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-01 11:25:05 +0800

    lineage-23.0: 26-01

--- lineageos-23.0/daisy_a16_recipe.sh
+++ lineageos-23.0/daisy_a16_recipe.sh
@@ -14,20 +14,4 @@ git clone --depth=1 --branch lineage-22.2 https://github.com/LineageOS/android_h
 
 # NOTE:
 # kernel tree
-# git clone --depth=1 --branch lineage-23.0-bpf-test https://github.com/ximi-daisy-test/android_kernel_xiaomi_msm8953/ kernel/xiaomi/msm8953
-
-# for the kernel use something like zyc clang 22, so yes external clang
-# https://github.com/ZyCromerZ/Clang/releases
-# extract and then add its path to TARGET_KERNEL_CLANG_PATH on device/xiaomi/msm8953-common/BoardConfigCommon.mk
-
-# -- add these too and remove /external on build/soong/ui/build/androidmk_denylist.go
-git clone --depth=1 https://github.com/LineageOS/android_external_ant-wireless_ant_client.git external/ant-wireless/ant_client
-git clone --depth=1 https://github.com/LineageOS/android_external_ant-wireless_ant_native.git external/ant-wireless/ant_native
-git clone --depth=1 https://github.com/LineageOS/android_external_ant-wireless_ant_service.git external/ant-wireless/ant_service
-
-# apply this fix on external/ant-wireless/ant_service
-# https://github.com/LineageOS/android_external_ant-wireless_ant_service/commit/808676c8bf84dddec22c76fdff880e6e394c366e
-
-# for recovery, add bypasses
-# apply on bootable/recovery
-# https://github.com/ximi-daisy-test/android_bootable_recovery/commit/74a50ca6db16ac4c6b7353e9d50f035e19891ff8
+# git clone --depth=1 --branch lineage-23.0-bpf-mini https://github.com/ximi-daisy-test/android_kernel_xiaomi_msm8953/ kernel/xiaomi/msm8953
